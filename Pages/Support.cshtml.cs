779ce1b475e082095c73f43fb9922239849eff4c
wonderlandkid1990 <honlovehim@gmail.com>
2026-06-11 00:40:27 +0800

    Add support page.

---
+++ Pages/Support.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NailArtHub.Pages
+{
+    public class SupportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
